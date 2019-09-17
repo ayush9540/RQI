ac2681ea602989c4b4bf00650c6549177f2b98a7
ayush.gupta <ayush.gupta@impelsys.com>
2019-09-17 19:12:44 +0530

    code updated

--- main.js
+++ main.js
@@ -1,50 +1,45 @@
-// var assignCategory = {
-//     O: ["1"],
-//     R: ["abc", "def", "ghi", "jkl"],
-//     I: ["bob", "mike", "leo", "raph"],
-//     D: ["dummy", "dummy"]
-// }
-
-// function changecat(value) {
-//     if (value.length == 0) document.getElementById("category").innerHTML = "<option></option>";
-//     else {
-//         var catOptions = "";
-//         for (categoryId in assignCategory[value]) {
-//             catOptions += "<option>" + assignCategory[value][categoryId] + "</option>";
-
-//         }
-//         document.getElementById("category").innerHTML = catOptions;
-//     }
-//     $('#unitName').removeClass('d-none');
-//     
-// }
-
 $(document).ready(function() {
     $('.card-link').click(function() {
         $('#searchBox').toggleClass('d-none');
         $('#clear').toggleClass('d-none');
     });
-    // $('#orgLevel').on('change', function() {
-    //     $('#regionName').toggleClass('d-none', this.value != 'B');
-    //     if ($('#regionName').hasClass('d-none')) {
-    //         $('#unitName').removeClass('d-none')
-    //     } else {
-    //         $('#unitName').addClass('d-none')
-    //     };
-    // });
-    document.getElementById('orgLevel').addEventListener('change', function () {
+    document.getElementById('orgLevel').addEventListener('change', function() {
         $('#clear').removeClass('d-none');
-
+        $('.card-link').removeClass('inactive');
+        $('#searchbtn').removeClass('disabled');
         var untName = this.value == 1 ? 'block' : 'none';
-        document.getElementById('unitName').style.display = untName;       
+        document.getElementById('unitName').style.display = untName;
         var rgnName = this.value == 2 ? 'block' : 'none';
         document.getElementById('regionName').style.display = rgnName;
         var instName = this.value == 3 ? 'block' : 'none';
         document.getElementById('instituteName').style.display = instName;
         var deptName = this.value == 4 ? 'block' : 'none';
-        document.getElementById('departmentName').style.display = deptName;       
-    });
+        document.getElementById('departmentName').style.display = deptName;
 
+        if (this.value == '2') {
+            $("#regionInsidePanel").hide();
+        } else {
+            $("#regionInsidePanel").show();
+        }
+
+        if (this.value == '3') {
+            $("#instituteInsidePanel").hide();
+            $("#regionInsidePanel").hide();
+        } else {
+            $("#instituteInsidePanel").show();
+        }
+
+        if (this.value == '4') {
+            $('.card-link').addClass('inactive');
+            $("#deptInsidePanel").hide();
+            $("#instituteInsidePanel").hide();
+            $("#regionInsidePanel").hide();
+            $("#collapseOne").removeClass("in").addClass("collapse")
+        } else {
+            $("#deptInsidePanel").show();
+            $('.card-link').removeClass('inactive');
+        }
+    });
 });
 
 $(function() {
