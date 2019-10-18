7ad0f0ec8b0e683ee0556e98650659814bb503dc
ayush.gupta <ayush.gupta@impelsys.com>
2019-10-18 17:44:34 +0530

    UI updated

--- main.js
+++ main.js
@@ -10,53 +10,16 @@ $(document).ready(function() {
             $("#searchBox").show();
             $("#clear").show();
         }
-        // var els = $(".ms-options label");
-        // els.addClass("mightOverflow");
-        // $(".ms-selectall").trigger("click");
-        // $("#instituteInsidePanel span").html("5 selected");
-        // $("#deptInsidePanel span").html("5 selected");
-        // $("#regionInsidePanel span").html("5 selected");
-        // $("#regionName span").html("Select Region(s)");
-        // $("#instituteName span").html("Select Institute(s)");
-        // $("#departmentName span").html("Select Department(s)");
+
     });
-    // $('select[multiple]').click(function() {
-    //     $(".ms-options > label").addClass("mightOverflow");
-    //     $(".ms-options-wrap.ms-active .ms-options ul li").addClass("mightOverflow");
-
-    // });
-    // $(".ms-options-wrap.ms-active .ms-options ul li").addClass("mightOverflow");
-    //$(".ms-options > label").addClass("mightOverflow");
-
-    // $(".ms-options-wrap.ms-active").find("li").addClass("test");
-
-    // var x = $(".ms-options-wrap");
-    // if (!x.hasClass("ms-active")) {
-    //     $(x).addClass("test");
-    //     alert("hi");
-    // }
-
-    // $.fn.tooltipOnOverflow = function(options) {
-    //     $(this).on("mouseenter", function() {
-    //         if (this.offsetWidth < this.scrollWidth) {
-    //             options = options || { placement: "auto" }
-    //             options.title = $(this).text();
-    //             $(this).tooltip(options);
-    //             $(this).tooltip("show");
-    //         } else {
-    //             if ($(this).data("bs.tooltip")) {
-    //                 $tooltip.tooltip("hide");
-    //                 $tooltip.removeData("bs.tooltip");
-    //             }
-    //         }
-    //     });
-    // };
-
-    // $('.mightOverflow').tooltipOnOverflow();
-
-
 
     /* clear & search from input search start */
+    $('#cancel').click(function() {
+        $("#inputSearch").show();
+        $(".close-icon").hide();
+        $("#searchbox_name_email").val("");
+    });
+
     $("#inputSearch").click(function() {
         $(this).hide();
         $("#searchbox_name_email").focus();
@@ -64,13 +27,16 @@ $(document).ready(function() {
     $("#searchbox_name_email").click(function() {
         $("#inputSearch").hide();
     });
-    $(".close-icon").click(function() {
-        $("#inputSearch").show();
-    });
+
+
     $("#searchbox_name_email").focusout(function() {
         if ($(this).val().length === 0) {
             $("#inputSearch").show();
+            $(".close-icon").hide();
         }
+    });
+    $("#searchbox_name_email").focusin(function() {
+        $(".close-icon").show();
     });
     /* clear & search from input search end */
 
@@ -119,14 +85,12 @@ $(document).ready(function() {
 });
 
 $(function() {
+    $(".close-icon").hide();
 
     $("#clear,#clear-inCollapsible").bind("click", function() {
         $("select")[0].selectedIndex = 0;
         $('select[multiple]').multiselect('reset');
     });
-
-    // var x = $(".ms-options-wrap.ms-active");
-    // $(x).closest("li").addClass("testClass");
 
     $('select[multiple].active.region').multiselect({
         columns: 1,
@@ -151,6 +115,16 @@ $(function() {
     $('select[multiple].active.dept').multiselect({
         columns: 1,
         placeholder: 'Select Department(s)',
+        search: true,
+        searchOptions: {
+            'default': 'Search here...'
+        },
+        selectAll: true
+    });
+
+    $('select[multiple].active.course').multiselect({
+        columns: 1,
+        placeholder: 'Select Course(s)',
         search: true,
         searchOptions: {
             'default': 'Search here...'
